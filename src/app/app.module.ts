91f542982ec28ac2fe38a71083d258a1495e8876
jess <jlbachel@umich.edu>
2019-10-02 22:19:54 -0400

    mat imports no longer crash the system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActionsComponent } from './actions/actions.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+  MatCheckboxModule, 
+  MatListModule, 
+  MatCardModule } from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +20,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
