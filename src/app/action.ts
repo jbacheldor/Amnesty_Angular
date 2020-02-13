5c1e27a5a8e05ee69ed719f0778e14f5d8de808c
jess <jlbachel@umich.edu>
2020-02-12 22:50:33 -0500

    pictures on icons & logo in place

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,4 +1,5 @@
 export class Action {
 	name: string;
 	check: boolean;
+	url: string;
 }
--- src/app/actions-list.ts
+++ src/app/actions-list.ts
@@ -1,17 +1,20 @@
 import {Action} from './action';
 
 export const ACTIONES: Action[] = [
-	{ name: 'International Justice', check: false },
-	{ name: 'Death Penalty', check: false },
-	{ name: 'Corporate Accountability', check: false },
-	{ name: 'United Nations', check: false },
-	{ name: 'Discrimination', check: false },
-	{ name: 'Living in Poverty', check: false },
-	{ name: 'Indigenous People', check: false },
-	{ name: 'Sexual and Reproductive Rights', check: false },
-	{ name: 'Freedom of Expression', check: false },
-	{ name: 'Arms Control', check: false },
-	{ name: 'People on the Move', check: false },
-	{ name: 'Armed Conflict', check: false },
-	{ name: 'Torture', check: false }
+	{ name: 'International Justice', check: false, url: '/../../assets/images/international-justice.png'},
+	{ name: 'Death Penalty', check: false, url: '/../../assets/images/death-penalty.png'},
+	{ name: 'Corporate Accountability', check: false, url: '/../../assets/images/corporate-accountability.png'},
+	{ name: 'United Nations', check: false, url: '/../../assets/images/un.png'},
+	{ name: 'Discrimination', check: false, url: '/../../assets/images/discrimination.png' },
+	{ name: 'Living with Dignity', check: false, url: '/../../assets/images/living-with-diginity.png' },
+	{ name: 'Indigenous People', check: false, url: '/../../assets/images/indigenous-people.png' },
+	{ name: 'Sexual and Reproductive Rights', check: false, url: '/../../assets/images/sexual-and-reproductive-rights.png' },
+	{ name: 'Freedom of Expression', check: false, url: '/../../assets/images/freedom-of-expression-association-assembly.png' },
+	{ name: 'Arms Control', check: false, url: '/../../assets/images/arms-control.png'},
+	{ name: 'People on the Move', check: false, url: '/../../assets/images/people-on-the-move.png'},
+	{ name: 'Armed Conflict', check: false, url: '/../../assets/images/armed-conflict.png'},
+	{ name: 'Torture', check: false, url: '/../../assets/images/torture.png' },
+	{ name: 'Climate Change', check: false, url: '/../../assets/images/climate-change.png' },
+	{ name: 'Detention and Imprisonment', check: false, url: '/../../assets/images/detention-and-imprisonment.png' },
+	{ name: 'Disapearances', check: false, url: '/../../assets/images/dissapearances.png' },
 ];
